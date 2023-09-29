45d2233e0ab459a6458a6354eec28329a0d433b2
auriceliof <auricelio.suporte@hotmail.com>
2023-09-29 20:19:01 -0300

    03-19 BCrypt password encoder

--- spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$bda2wDC3wNalO8koNAL69u0/5w23tx2JH6R25KYsWnh5Z5Omefavq');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$bda2wDC3wNalO8koNAL69u0/5w23tx2JH6R25KYsWnh5Z5Omefavq');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
